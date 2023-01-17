3148e969e404ad847fa260b59678f565b21a04ef
Your Name <you@example.com>
2023-01-17 18:54:55 +0100

    He anadido minus

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ from utils import mayus
 frase = "Quiero esto en mayuscula"
 resultado = mayus(l_string=frase)
 print(resultado)
+
+from utils import minus
+frase2 = "Quiero esTo en minusculA"
+resultado2 = minus(l_string=frase2)
+print(resultado2)
